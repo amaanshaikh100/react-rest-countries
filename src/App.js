c52e0ef855931be1518ee76a31028354001f3f12
YOUR_USERNAME <amaanshaikh59442@outlook.com>
2023-11-29 18:54:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
+export default function App() {
+  const [country, setCountry] = useState("Australia");
+  const [countryDetails, setCountryDetails] = useState([]);
+
+  useEffect(function() {
+    async function fetchCountries() {
+      try {
+        const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+
+        if (!res.ok) {
+          throw new Error("Something went wrong.");
+        }
+
+        const data = await res.json();
+        if (data.Response === "False") {
+          throw new Error("Country not found");
+        }
+
+        setCountryDetails(data[0]);
+
+      } catch(err) {
+        console.log("There's an error", err);
+      }
+    }
+
+    fetchCountries();
+  }, [country]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Form country={country} setCountry={setCountry} />
+      <Country countryDetails={countryDetails} />
+    </>
+  )
 }
 
-export default App;
+function Form({ country, setCountry }) {
+  return (
+    <form>
+      <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} placeholder="India..." />
+      <button>Submit</button>
+    </form>
+  )
+}
+
+function Country({ countryDetails }) {
+  const borders = countryDetails.borders ? countryDetails.borders.map((border) => border) : null;
+  const population = countryDetails.population;
+
+  return (
+    <div>
+      <img src={countryDetails.flags?.png} alt="countres flag" />
+      <p>{countryDetails.altSpellings}</p>
+      <span>Population: {population}</span>
+      <ul key={countryDetails}>{borders ? borders.map((border, i) => <li key={i}>{border}</li>) : "This country has no borders."}</ul>
+    </div>
+  )
+}
